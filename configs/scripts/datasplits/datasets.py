01f5166175892cc33d2914d59fbb1c4cd32f3104
pattonw <pattonw@hhmi.org>
2023-05-10 00:34:36 -0400

    load sparse datasets

--- configs/scripts/datasplits/datasets.py
+++ configs/scripts/datasplits/datasets.py
@@ -5,15 +5,18 @@ from dacapo.experiments.datasplits.datasets import RawGTDatasetConfig
 from dacapo.experiments.datasplits.datasets.arrays import (
     ZarrArrayConfig,
     IntensitiesArrayConfig,
+    OnesArrayConfig,
 )
 
 from funlib.geometry import Coordinate
 
+import numpy as np
 import click
 
 import logging
 from pathlib import Path
 import yaml
+import random
 
 
 @click.group()
@@ -31,7 +34,6 @@ def cli(log_level):
 @cli.command()
 @click.option("--force/--no-force", default=False)
 def update(force):
-
     # Load yamls
     yaml_root_dir = Path("configs/yamls/zebrafish")
     assert yaml_root_dir.exists(), f"{yaml_root_dir} does not exist!"
@@ -48,31 +50,74 @@ def update(force):
     dataset_yaml = yaml_root_dir / "datasets.yaml"
     datasets = yaml.safe_load(dataset_yaml.open("r").read())
 
-    def create_dataset(crop, index, organelle):
+    def create_dataset(sample, organelle):
         raw_config = IntensitiesArrayConfig(
-            f"zebrafish_raw_{crop}_{index}",
+            f"zebrafish_raw_{sample}",
             ZarrArrayConfig(
-                f"zebrafish_raw_{crop}_{index}_uint8",
+                f"zebrafish_raw_{sample}_uint8",
                 constants["dataset_container"],
-                constants["raw_dataset"].format(crop=crop, index=index),
+                constants["raw_dataset"].format(sample=sample),
             ),
             0,
             255,
         )
-        gt_config = ZarrArrayConfig(
-            f"zebrafish_gt_{crop}_{index}_{organelle}",
-            constants["dataset_container"],
-            constants["gt_dataset"].format(crop=crop, index=index, organelle=organelle),
-        )
+
         mask_config = ZarrArrayConfig(
-            f"zebrafish_mask_{crop}_{index}_{organelle}",
+            f"zebrafish_mask_{sample}_{organelle}",
             constants["dataset_container"],
-            constants["mask_dataset"].format(
-                crop=crop, index=index, organelle=organelle
-            ),
+            constants["mask_dataset"].format(sample=sample, organelle=organelle),
         )
+        if Path(
+            constants["dataset_container"],
+            constants["gt_dataset"].format(sample=sample, organelle=organelle),
+        ).exists():
+            gt_config = ZarrArrayConfig(
+                f"zebrafish_gt_{sample}_{organelle}",
+                constants["dataset_container"],
+                constants["gt_dataset"].format(sample=sample, organelle=organelle),
+            )
+        else:
+            gt_config = OnesArrayConfig(
+                f"zebrafish_gt_full_{sample}_{organelle}", mask_config
+            )
+            gt_config = IntensitiesArrayConfig(
+                f"zebrafish_gt_empty_{sample}_{organelle}", gt_config, 1, 2
+            )
+        points_path = Path(
+            constants["dataset_container"],
+            constants["points_dataset"].format(sample=sample, organelle=organelle)
+            + ".npz",
+        )
+        if points_path.exists():
+            points = np.load(points_path)["arr_0"]
+            points = set(Coordinate(*coords) for coords in zip(*points))
+            offsets = [
+                Coordinate(1, 0, 0),
+                Coordinate(-1, 0, 0),
+                Coordinate(0, 1, 0),
+                Coordinate(0, -1, 0),
+                Coordinate(0, 0, 1),
+                Coordinate(0, 0, -1),
+            ]
+            decimated_points = []
+            for point in points:
+                if (
+                    sum([point + offset in points for offset in offsets])
+                    < len(offsets) - 1
+                ):
+                    decimated_points.append(Coordinate(*tuple(point)[::-1]))
+            print(sample, organelle, len(points), len(decimated_points))
+            points = decimated_points
+            points = random.choices(points, k=2000)
+        else:
+            points = None
         return RawGTDatasetConfig(
-            f"zebrafish_{crop}_{index}_{organelle}", raw_config, gt_config, mask_config
+            f"zebrafish_{sample}_{organelle}",
+            raw_config=raw_config,
+            gt_config=gt_config,
+            mask_config=mask_config,
+            sample_points=points,
+            weight=5 if "-" in sample else 1,
         )
 
     config_store = create_config_store()
@@ -81,12 +126,10 @@ def update(force):
         datasplit_config = TrainValidateDataSplitConfig(
             f"zebrafish_{organelle}",
             train_configs=[
-                create_dataset(crop, index, organelle)
-                for crop, index in datasets["train"]
+                create_dataset(sample, organelle) for sample in datasets["train"]
             ],
             validate_configs=[
-                create_dataset(crop, index, organelle)
-                for crop, index in datasets["validate"]
+                create_dataset(sample, organelle) for sample in datasets["validate"]
             ],
         )
         datasplit_configs.append(datasplit_config)
